4c60d6ae73904d88a16d51fb8daf0bb944aa4ec3
jasonzhanhuang <huangzhan@hotmail.co.uk>
2014-12-13 15:16:02 +0000

    Revert 9c9decd..e06678d
    This rolls back to commit 9c9decd8e5d5cb5a1c4cf8543f580c74faba6262.

--- asSemantics/Agent.java
+++ asSemantics/Agent.java
@@ -485,8 +485,10 @@ public class Agent {
     /** add the initial beliefs in BB and produce the corresponding events */
     public void addInitialBelsInBB() throws RevisionFailedException {
         // Once beliefs are stored in a Stack in the BB, insert them in inverse order
-
-        for (int i=initialBels.size()-1; i >=0; i--) {
+    	
+    	//4. Belief addition position change
+    	for(int i=0;i<initialBels.size();i++) {
+        //for (int i=initialBels.size()-1; i >=0; i--) {
             Literal b = initialBels.get(i);
 
             // if l is not a rule and has free vars (like l(X)), convert it into a rule like "l(X) :- true."
