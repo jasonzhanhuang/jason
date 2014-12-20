16ea566e02a39bcdac612655f11f577da96fe67b
jasonzhanhuang <huangzhan@hotmail.co.uk>
2014-12-20 23:18:07 +0000

    10. (final) Test goal achievement event semantics
    change

--- asSemantics/TransitionSystem.java
+++ asSemantics/TransitionSystem.java
@@ -747,7 +747,8 @@ public class TransitionSystem {
             		conf.C.addEvent(evt);
             		confP.step = State.StartRC;
             	} else {
-            		
+                    if (logger.isLoggable(Level.FINE)) logger.fine("Test '"+h+"' failed ("+h.getSrcInfo()+").");
+                    generateGoalDeletion(conf.C.SI, JasonException.createBasicErrorAnnots("test_goal_failed", "Failed to test '"+h+"'"));
             	}
             }
             break;
